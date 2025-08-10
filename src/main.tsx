d48c32bec18cbd4dc0c14ce00dd75a8f2e80f903
Dillon Cassidy <dillon.cassidy@equipmentshare.com>
2025-08-10 15:35:18 -0500

    patch

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App'
-import { BrowserRouter } from 'react-router-dom'
-import './styles.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import './styles.css';
 
-const root = document.getElementById('root')
-if (!root) throw new Error('Root element #root not found')
+const root = document.getElementById('root');
+if (!root) throw new Error('Root element #root not found');
 
 createRoot(root).render(
   <StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </StrictMode>
-)
+  </StrictMode>,
+);
