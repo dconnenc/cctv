2cf97176dfd9a4b714f8754c9916f2105db7f6a6
Jared Shay <jared.shay@gmail.com>
2025-10-23 13:54:35 -0500

    Setup tv view (#36)
    * Show TV view in preview
    
    * Add TV page

--- app/frontend/Pages/Manage/ContextView/ContextView.tsx
+++ app/frontend/Pages/Manage/ContextView/ContextView.tsx
@@ -1,7 +1,8 @@
 import { Panel } from '@cctv/core';
-import { Block, ParticipantSummary } from '@cctv/types';
+import { Block, Experience, ParticipantSummary } from '@cctv/types';
 
 import BlockPreview from '../BlockPreview/BlockPreview';
+import QRCodeDisplay from '../QRCodeDisplay/QRCodeDisplay';
 
 import styles from './ContextView.module.scss';
 
@@ -9,9 +10,29 @@ interface ContextViewProps {
   block?: Block;
   participant?: ParticipantSummary;
   emptyMessage: string;
+  tvView?: Experience;
+  viewMode: 'tv' | 'participant';
 }
 
-export default function ContextView({ block, participant, emptyMessage }: ContextViewProps) {
+export default function ContextView({
+  block,
+  participant,
+  emptyMessage,
+  tvView,
+  viewMode,
+}: ContextViewProps) {
+  if (viewMode === 'tv' && tvView) {
+    const tvBlock = tvView.blocks[0];
+
+    return (
+      <Panel title="TV View">
+        <div className={styles.preview}>
+          {tvBlock ? <BlockPreview block={tvBlock} /> : <QRCodeDisplay experience={tvView} />}
+        </div>
+      </Panel>
+    );
+  }
+
   return (
     <Panel title="Current">
       <div className={styles.preview}>
