5d9a336e4647515497a2d071e22d7b06bbe5dc98
Jared Shay <jared.shay@gmail.com>
2026-05-27 09:18:27 -0500

    Fix out dated spec

--- spec/factories/experience_poll_submissions_factory.rb
+++ spec/factories/experience_poll_submissions_factory.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :experience_poll_submission do
     association :experience_block
-    association :user
+    association :experience_participant
     answer { { "selectedOptions" => ["option_a"] } }
   end
 end
